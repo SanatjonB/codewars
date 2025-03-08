f3f1833f45ff4be78026af9c12526aae35a9fb9f
Sanatjon Burkhanov <sburhanov1977@gmail.com>
2025-03-07 23:56:23 -0500

    problem2

---
+++ problem1.java
@@ -0,0 +1,21 @@
+public class problem1
+{
+    public static int[] countPositivesSumNegatives(int[] input) {
+        if (input == null || input.length == 0) {
+            return new int[0]; // Return an empty array for null or empty input
+        }
+
+        int countPositives = 0;
+        int sumNegatives = 0;
+
+        for (int num : input) {
+            if (num > 0) {
+                countPositives++;
+            } else if (num < 0) {
+                sumNegatives += num;
+            }
+        }
+
+        return new int[]{countPositives, sumNegatives};
+    }
+}
